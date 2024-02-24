3f356a71f30b2730fb0b0c605f9c4f15e98540d3
unknown <yesidkiigut@gmail.com>
2024-02-24 17:35:00 -0500

    se crean stylos y se agregan componentes topbar,botonbar,pokelist

---
+++ pokedex/src/components/BottomBar.jsx
@@ -0,0 +1,15 @@
+import "./BottonBar.css"
+
+export function BottomBar() {
+    return(<div className="bottom">
+    <div className="bottom-btn">
+      <p className="bg-circle-white">A</p>
+      <p>See Details</p>
+    </div>
+    <div className="bottom-btn">
+      <p className="bg-circle-white">X</p>
+      <p>Habitat</p>
+    </div>
+   
+  </div>)
+   }
---
+++ pokedex/src/components/Pokelist.jsx
@@ -0,0 +1,3 @@
+export function Pokelist() {
+    return(<h1>PokeList</h1>)
+   }
